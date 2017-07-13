03c36b68ca9c0612b6062e09ccec96a5cc6e712f
RecursiveIteration <James.C.Woodson@gmail.com>
2017-07-13 11:52:51 -0700

    separated bio info into it's own data file, wrote JQuery methods to fill it in webpage

---
+++ js/bioData.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var bioData = {
+  name: 'James C. Woodson',
+  imageUrl: 'images/james.jpg',
+  jobHistory: [
+    ['Western Fisheries Research Center', 'Research Technician'],
+    ['MDS Pharma Services', 'Assistant Scientist']
+  ],
+  skills: [
+    'JavaScript',
+    'Java',
+    'R',
+    'HTML',
+    'CSS',
+  ],
+  bio: [
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel faucibus risus. Vivamus fermentum, urna eget placerat auctor, elit purus vestibulum ligula, vitae venenatis ipsum quam ut leo. Nunc vel tempor nisi. Sed ultricies hendrerit magna, sed vehicula tellus convallis eget. Maecenas viverra dolor eget euismod mattis. Phasellus at nisl diam. Nam venenatis, libero vel egestas posuere, neque diam gravida ex, a molestie nisi massa ut risus.',
+    'Maecenas laoreet eleifend ex nec fringilla. Nulla ultrices vestibulum mattis. Aliquam auctor ipsum non eros feugiat tincidunt. Praesent efficitur eros a consequat lacinia. Fusce blandit semper mi vel tempus. Ut sit amet risus urna. Aenean dapibus faucibus mattis. Praesent hendrerit viverra tincidunt. Sed interdum ullamcorper aliquam. In vitae mollis massa. Aliquam pellentesque nibh eget augue consectetur efficitur. Duis nec nulla at augue facilisis lacinia. Vestibulum ut malesuada mi.',
+    'Etiam vestibulum lacinia tempus. Nulla facilisi. Sed laoreet est sed imperdiet mollis. Suspendisse a vulputate dolor, non faucibus orci. Sed venenatis, quam at convallis varius, diam odio efficitur ex, nec consequat ex lacus a eros. Donec sed viverra lectus. Suspendisse lacinia sem et ultricies vehicula. Mauris efficitur pretium lacus, sagittis sollicitudin nisi tempus molestie. Suspendisse porttitor lectus fringilla tellus semper, sed gravida odio lacinia. Vestibulum non odio elit. Curabitur ac ex ultricies, volutpat ipsum vel, imperdiet lacus. Donec eget nisi vel ante imperdiet pretium vel vel sem.'
+  ]
+};
