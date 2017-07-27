ddd906d22e2e873fa3e043a69e555c4f8e16cdd8
RecursiveIteration <James.C.Woodson@gmail.com>
2017-07-26 22:31:13 -0700

    projectModel loads project data, moved bioData into .json file

--- public/js/models/bioModel.js
+++ public/js/models/bioModel.js
@@ -3,6 +3,5 @@
 var app = app || {};
 
 (function (module) {
-  let bioModelCheck = 'bioModelAttached';
-  module.bioModelCheck = bioModelCheck;
+  
 })(app);
--- public/js/models/projectsModel.js
+++ public/js/models/projectsModel.js
@@ -3,6 +3,20 @@
 var app = app || {};
 
 (function (module) {
-  let projectsModelCheck = 'projectsModelAttached';
-  module.projectsModelCheck = projectsModelCheck;
+  function Project (projectObj) {
+    this.title = projectObj.title;
+    this.githubUrl = projectObj.githubUrl;
+    this.deploymentUrl = projectObj.deploymentUrl;
+    this.imageUrl = projectObj.imageUrl;
+    this.synopsis = projectObj.synopsis;
+  }
+
+  Project.projects = [];
+
+  Project.loadProjects = function () {
+    $.getJSON('/data/projects.json').then(function(data) {
+      data.map((project) => Project.projects.push(new Project(project)));
+    });
+  }
+  module.Project = Project;
 })(app);
